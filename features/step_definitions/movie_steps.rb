14635c7c7d3817a3531706f5eb5e544b4a736b1f
Robert Shane <rshane@berkeley.edu>
2012-09-27 23:49:23 -0700

    finish 2c - 3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -29,4 +29,64 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list = rating_list.split(%r{,\s*})
+  rating_list.each do |rating|
+    rating = 'ratings_' + rating
+    if uncheck
+      uncheck(rating)
+    else
+      check(rating)
+    end
+  end
+end
+
+
+Then /^the "([^"]*)" checkbox(?: within (.*))? should be checked$/ do |label, parent|
+  with_scope(parent) do
+    field_checked = find_field(label)['checked']
+    if field_checked.respond_to? :should
+      field_checked.should be_true
+    else
+      assert field_checked
+    end
+  end
+end
+
+Then /^the "([^"]*)" checkbox(?: within (.*))? should not be checked$/ do |label, parent|
+  with_scope(parent) do
+    field_checked = find_field(label)['checked']
+    if field_checked.respond_to? :should
+      field_checked.should be_false
+    else
+      assert !field_checked
+    end
+  end
+end
+
+Then /^(?:|I )should (not )?see the following movies: (.*)/ do |not_see, text|
+  movie_list = text.split(%r{, })
+  movie_list.each do |movie|
+    if not_see
+      if page.respond_to? :should
+        page.should have_no_content(movie)
+      else
+        assert page.has_no_content?(movie)
+      end
+    else
+      if page.respond_to? :should
+        page.should have_content(movie)
+      else
+        assert page.has_content?(movie)
+      end
+    end
+  end
+end
+
+And /^(?:|I )press on the homepage "([^"]*)"$/ do |button|
+  if button == "Refresh"
+    button = 'ratings_submit'
+    click_button(button)
+  else
+    click_button(button)
+  end
 end
